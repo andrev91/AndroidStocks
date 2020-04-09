d7a314b9802aaf05ef9c57507cdfed5948ad051c
avicente91 <andrevicente1991@gmail.com>
2020-04-09 00:21:23 -0400

    Updates to the dependency for firebase auth. Adding first login attempt

--- app/src/main/java/andre/vicente/yahoostocks/data/LoginDataSource.kt
+++ app/src/main/java/andre/vicente/yahoostocks/data/LoginDataSource.kt
@@ -1,7 +1,9 @@
 package andre.vicente.yahoostocks.data
 
 import andre.vicente.yahoostocks.data.model.User
-import java.io.IOException
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+
 
 /**
  * Class that handles authentication w/ login credentials and retrieves user information.
@@ -9,6 +11,10 @@ import java.io.IOException
 class LoginDataSource {
 
     fun login(username: String, password: String): Result<User> {
+        val mAuth : FirebaseAuth = FirebaseAuth.getInstance()
+        mAuth.signInWithEmailAndPassword("tehuberz@yahoo.com","pokemon").addOnCompleteListener {
+            Log.d(this.javaClass.name,"Login Successful!!")
+        }
 
         return Result.Success(User())
     }
